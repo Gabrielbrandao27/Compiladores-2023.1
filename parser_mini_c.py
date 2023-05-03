53249c67c19460fb39901ef0cd50eb4571c2d0a1
Gabriel Brandão <gabriel.brandao@hashdex.com>
2023-05-03 18:07:45 -0300

    Reduzindo BNF e Firsts Table + mexendo com parser

--- parser_mini_c.py
+++ parser_mini_c.py
@@ -1,1 +1,12 @@
 # Um parser que recebe a lista de tokens e tipos e retorna a árvore sintática
+
+tokens = [['(', '('], [')', ')'], ['{', '{'], ['}', '}'], [',', ','], ['.', '.'], [';', ';'], 
+         ['=', '='], ['==', '=='], ['<', '<'], ['>', '>'], ['<=', '<='], ['>=', '>='], 
+         ['!=', '!='], ['+', '+'], ['-', '-'], ['*', '*'], ['/', '/'], ['1234', 'int'], 
+         ['12.34', 'float'], ['for', 'for'], ['while', 'while'], ['if', 'if'], 
+         ['else', 'else'], ['var1', 'identifier'], ['123', 'number']]
+
+pilha = []
+
+for token in tokens:
+    pilha.append[token[0]]
