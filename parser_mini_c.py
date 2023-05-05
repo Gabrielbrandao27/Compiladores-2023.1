155acc8cb96693773236726d885387852a9b0ab3
Gabriel Brandão <gabriel.brandao@hashdex.com>
2023-05-05 20:15:51 -0300

    avanço no parser

--- parser_mini_c.py
+++ parser_mini_c.py
@@ -1,71 +1,4 @@
 # Um parser que recebe a lista de tokens e tipos e retorna a árvore sintática
-
-tokens = [['(', '('], [')', ')'], ['{', '{'], ['}', '}'], [',', ','], ['.', '.'], [';', ';'], 
-         ['=', '='], ['==', '=='], ['<', '<'], ['>', '>'], ['<=', '<='], ['>=', '>='], 
-         ['!=', '!='], ['+', '+'], ['-', '-'], ['*', '*'], ['/', '/'], ['1234', 'int'], 
-         ['12.34', 'float'], ['for', 'for'], ['while', 'while'], ['if', 'if'], 
-         ['else', 'else'], ['var1', 'identifier'], ['123', 'number']]
-
-simbolos = {
-    0: "Function",
-    1: "ArgList",
-    2: "Arg",
-    3: "Declaration",
-    4: "Type",
-    5: "IdentList",
-    6: "Stmt",
-    7: "ForStmt",
-    8: "OptExpr",
-    9: "whileStmt",
-    10: "IfStmt",
-    11: "ElsePart",
-    12: "CompoundStmt",
-    13: "StmtList",
-    14: "Expr",
-    15: "Rvalue",
-    16: "Compare",
-    17: "Mag",
-    18: "Term",
-    19: "Factor"
-}
-
-tokens_struct = {
-    0: "identifier",
-    1: "number",
-    2: "int",
-    3: "float",
-    4: "if",
-    5: "else",
-    6: "for",
-    7: "while",
-    8: ",",
-    9: ".",
-    10: ";",
-    11: "{",
-    12: "}",
-    13: "(",
-    14: ")",
-    15: "=",
-    16: "==",
-    17: "<",
-    18: ">",
-    19: "<=",
-    20: ">=",
-    21: "!=",
-    22: "-",
-    23: "+",
-    24: "*",
-    25: "/",
-}
-
-lista_regras = [[None] * len(tokens_struct) for i in range(len(simbolos))]
-
-for i in range(len(simbolos)):
-    for j in range(len(tokens_struct)):
-        lista_regras[i][j] = "ok"
-
-#print(lista_regras)
-
 pilha = []
 
 def vazia(pilha):
@@ -73,9 +6,6 @@ def vazia(pilha):
 
 def empilhar(pilha, item):
     return pilha.append(item)
-
-def remover(pilha, item):
-    return pilha.pop(item)
 
 def topo(pilha):
     return pilha[-1]
@@ -90,13 +20,100 @@ def posicao(pilha, item):
 def terminal(item):
     if item in tokens_struct:
         return True
+    
+tokens = [['(', '('], [')', ')'], ['{', '{'], ['}', '}'], [',', ','], ['.', '.'], [';', ';'], 
+         ['=', '='], ['==', '=='], ['<', '<'], ['>', '>'], ['<=', '<='], ['>=', '>='], 
+         ['!=', '!='], ['+', '+'], ['-', '-'], ['*', '*'], ['/', '/'], ['1234', 'int'], 
+         ['12.34', 'float'], ['for', 'for'], ['while', 'while'], ['if', 'if'], 
+         ['else', 'else'], ['var1', 'identifier'], ['123', 'number'], '$']
+
+simbolos = {
+    "Function" : "0",
+    "ArgList" : "1",
+    "Arg" : "2",
+    "Declaration" : "3",
+    "Type" : "4",
+    "IdentList" : "5",
+    "Stmt" : "6",
+    "ForStmt" : "7",
+    "OptExpr" : "8",
+    "whileStmt" : "9",
+    "IfStmt" : "10",
+    "ElsePart" : "11",
+    "CompoundStmt" : "12",
+    "StmtList" : "13",
+    "Expr" : "14",
+    "Rvalue" : "15",
+    "Compare" : "16",
+    "Mag" : "17",
+    "Term" : "18",
+    "Factor" : "19"
+}
+
+tokens_struct = {
+    'identifier': 0,
+    'number': 1,
+    'int': 2,
+    'float': 3,
+    'if': 4,
+    'else': 5,
+    'for': 6,
+    'while': 7,
+    ',': 8,
+    '.': 9,
+    ';': 10,
+    '{': 11,
+    '}': 12,
+    '(': 13,
+    ')': 14,
+    '=': 15,
+    '==': 16,
+    '<': 17,
+    '>': 18,
+    '<=': 19,
+    '>=': 20,
+    '!=': 21,
+    '-': 22,
+    '+': 23,
+    '*': 24,
+    '/': 25,
+}
+
+lista_regras = [[None] * len(tokens_struct) for i in range(len(simbolos))]
+
+for i in range(len(simbolos)):
+    for j in range(len(tokens_struct)):
+        lista_regras[i][j] = "ok"
+
+#print(lista_regras)
+
+
+iterator = iter(simbolos.keys())
+primeira_chave = next(iterator)
+
 
 empilhar(pilha, '$')
-empilhar(pilha, simbolos[0])
-print(pilha)
+empilhar(pilha, primeira_chave)
 
-for token in tokens:
+regra = ''
+
+for index, token in enumerate(tokens):
     token_atual = token
+    if index < len(tokens)-1:
+      prox_token = tokens[index+1]
+    else:
+        prox_token = None
 
-    while topo(pilha) != '$' and pilha[posicao(topo(pilha))-1] == '$':
-        if terminal(topo(pilha)) and token_atual 
+
+    while topo(pilha) != '$' and prox_token != '$':
+        
+        i = int(simbolos.get(topo(pilha)))
+        j = int(tokens_struct.get(token_atual[1]))
+        print("i: ", i, "j: ", j)
+        regra = lista_regras[i][j]
+        pilha.pop()
+        print("Regra: ", regra)
+        
+for r in reversed(regra):
+    empilhar(pilha, r)
+    print(pilha)
