b2f7ce866a7f0387045e785e33860af9da9eaf7a
Victor Fernandes <victor.faria@olxbr.com>
2023-05-18 20:29:33 -0300

    updating runner scanner

--- scanner/runner_scanner.py
+++ scanner/runner_scanner.py
@@ -33,6 +33,29 @@ def run_automata_inline_token(letter):
             pass
     return check_automatas_final_states(next_states, actual_automatas)
 
+def is_inline_token_name(token_name):
+    l = [x for x in inline_token if x[0] == token_name]
+
+    return len(l) == 1
+
+def run_automatas(actual_automatas, letter):
+    for i in range(len(automatas)):
+        if(automatas[i] in actual_automatas):
+            try:
+                n_s, b = run_automata(automatas[i][0], next_states[i], buffers[i], letter)
+                next_states[i] = n_s
+                buffers[i] = b
+            except Exception as e:
+                actual_automatas[i] = None
+
+def is_separator_token(letter):
+    if letter == '\n':
+        global count_line
+        global count_char
+        count_line += 1
+        count_char = 0
+
+    return letter in separator_token
 
 # main
 f_input_code = open("scanner/input_code.txt", "r")
@@ -54,29 +77,6 @@ text = input + " "
 count_char = 0
 count_line = 0
 
-def is_separator_token(letter):
-    if letter == '\n':
-        global count_line
-        global count_char
-        count_line += 1
-        count_char = 0
-
-    return letter in separator_token
-
-def is_inline_token_name(token_name):
-    l = [x for x in inline_token if x[0] == token_name]
-
-    return len(l) == 1
-
-def run_automatas(actual_automatas, letter):
-    for i in range(len(automatas)):
-        if(automatas[i] in actual_automatas):
-            try:
-                n_s, b = run_automata(automatas[i][0], next_states[i], buffers[i], letter)
-                next_states[i] = n_s
-                buffers[i] = b
-            except Exception as e:
-                actual_automatas[i] = None
 
 for letter in text:
     count_char += 1
@@ -86,7 +86,6 @@ for letter in text:
         token_name, index = check_automatas_final_states(next_states, actual_automatas)
 
         tokens.append([buffers[index], token_name])
-        print([buffers[index], token_name])
 
         buffers = [''] * len(automatas)
         next_states = [0] * len(automatas)
@@ -96,12 +95,9 @@ for letter in text:
     run_automatas(actual_automatas, letter)
 
     if check_array_has_only_null(actual_automatas):
-        #print(letter)
-
         token_name, index = check_automatas_final_states(next_states, automatas)
         tokens.append([buffers[index], token_name])
-        # inline_token_name, index = run_automata_inline_token(letter)
-        # tokens.append([letter, inline_token_name])
+
         buffers = [''] * len(automatas)
         next_states = [0] * len(automatas)
         actual_automatas = copy.deepcopy(automatas)
@@ -110,7 +106,6 @@ for letter in text:
         continue
 
 
-print(tokens)
 
 with open('scanner/result.text', 'w') as f:
     file_text = map(lambda x: ' '.join([x[0].strip(), x[1].strip()]).replace('\n', '').strip(), tokens)
